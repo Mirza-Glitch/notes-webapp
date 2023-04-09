26482e9ece6535fa7a12a5804532f06d3a9cd121
Mirza-Glitch <89267349+Mirza-Glitch@users.noreply.github.com>
2023-04-09 22:16:49 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -8,7 +8,9 @@ import "./index.css";
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <NotesProvider>
-      <App />
+      <BrowserRouter basename="/notes-webapp">
+        <App />
+      </BrowserRouter>
     </NotesProvider>
   </React.StrictMode>
 );
